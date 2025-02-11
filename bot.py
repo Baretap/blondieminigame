b428e261ce9ab5f1572aea4d81a91a72c281c730
Baretap <baretapgame.com>
2025-02-11 20:10:19 +0200

    Add blondie.png and update index.html

--- bot.py
+++ bot.py
@@ -1,23 +1,29 @@
-from telegram.ext import Updater, CommandHandler
+from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton
+from telegram.ext import Application, CommandHandler, ContextTypes
 import logging
+import asyncio
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 # Define the command handler
-def start(update, context):
-    update.message.reply_text('Welcome to the Blondie Mini Game! Click the Blondie image to score a point.')
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    keyboard = [
+        [InlineKeyboardButton("Play", url="https://baretap.github.io/BlondieClicker/")]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    
+    await update.message.reply_text('Welcome to the Blondie Mini Game! Click the button below to play the game:', reply_markup=reply_markup)
+    await asyncio.sleep(1)  # Add a delay of 1 second between messages
 
 def main():
-    # Replace '' with your actual bot token
-    updater = Updater("7348594119:AAFkfxTpTbby1BP7GEUvuXgt5Ss9roKyeJk", use_context=True)
+    # Replace 'YOUR_TOKEN' with your actual bot token
+    application = Application.builder().token("7348594119:AAFkfxTpTbby1BP7GEUvuXgt5Ss9roKyeJk").build()
 
-    dp = updater.dispatcher
-    dp.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("start", start))
 
-    updater.start_polling()
-    updater.idle()
+    application.run_polling()
 
 if __name__ == '__main__':
     main()
