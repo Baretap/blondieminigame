8613b4e683aaac06306f75cb5a591edf1bdc2b8a
Baretap <baretapgame.com>
2025-02-13 07:53:48 +0200

    Update and add all pages with Firebase integration and styles

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ import asyncio
 
 # Enable logging
 logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levellevelvelname)s - %(message)s',
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
 logger = logging.getLogger(__name__)
